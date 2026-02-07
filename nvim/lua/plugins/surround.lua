12f743fa6779103787bba506eec9aaabdffef2e8
Nicolas Gaubil <ngaubil@c1r14s1.marseille.42.school>
2026-02-07 16:13:13 +0100

    nouvelle config nvim et neofetch

---
+++ nvim/lua/plugins/surround.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        surrounds = {
+          ["("] = {
+            add = { "(", ")" },
+          },
+          ["["] = {
+            add = { "[", "]" },
+          },
+          ["{"] = {
+            add = { "{", "}" },
+          },
+          ['"'] = {
+            add = { '"', '"' },
+          },
+          ["'"] = {
+            add = { "'", "'" },
+          },
+        },
+      })
+    end,
+  },
+}
